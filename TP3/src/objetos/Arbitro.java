76d7d5f3e3f5d27717c35e01b418fc386fc28184
frderickk <federicofarias@outlook.com>
2021-10-26 01:21:04 -0300

    Comienzo de implementacion

--- TP3/src/objetos/Arbitro.java
+++ TP3/src/objetos/Arbitro.java
@@ -1,5 +1,24 @@
 package objetos;
 
 public class Arbitro {
+	
+	private int codigo;
+	private String nombre;
+	
+	
+	public Arbitro(int codigo, String nombre) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
 
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+	
 }
--- TP3/src/objetos/Equipo.java
+++ TP3/src/objetos/Equipo.java
@@ -1,5 +1,16 @@
 package objetos;
 
 public class Equipo {
+	
+	private String nombre;
+	
+	
+	public Equipo(String nombre) {
+		this.nombre = nombre;
+	}
 
+
+	public String getNombre() {
+		return nombre;
+	}
 }
