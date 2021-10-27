1f12eb78a699e0fbc969bbd5652600552c4734a3
EliasEspinillo <82783912+EliasEspinillo@users.noreply.github.com>
2021-10-27 20:06:10 -0300

    Se agregaron características a Equipo

--- TP3/src/objetos/Equipo.java
+++ TP3/src/objetos/Equipo.java
@@ -3,14 +3,30 @@ package objetos;
 public class Equipo {
 	
 	private String nombre;
+	private int [] arbitroAsignado;
 	
 	
-	public Equipo(String nombre) {
+	public Equipo(String nombre, int fechas) {
 		this.nombre = nombre;
+		this.arbitroAsignado = new int [fechas];
 	}
 
 
 	public String getNombre() {
 		return nombre;
 	}
+
+
+	public int [] getArbitroAsignado() {
+		return arbitroAsignado;
+	}
+
+
+	public void setArbitroAsignado(int [] arbitroAsignado) {
+		this.arbitroAsignado = arbitroAsignado;
+	}
+	
+	public void agregarArbitro(int a, int fecha) {
+		arbitroAsignado[fecha] = a;	
+		}
 }
